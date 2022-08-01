634c3ba686ff0a01bb20f77c3488e1405b56a973
fan12321 <s106062218@m106.nthu.edu.tw>
2022-08-01 17:12:27 +0800

    Updates

---
+++ precache-manifest.227270fecf00b96955f4d4e180c1abdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "639aafb5f2d1f6ef3f6a",
+    "url": "/TicTacToe/static/css/main.2a14c2f0.chunk.css"
+  },
+  {
+    "revision": "639aafb5f2d1f6ef3f6a",
+    "url": "/TicTacToe/static/js/main.56d28559.chunk.js"
+  },
+  {
+    "revision": "90328ccb9bdc72e64ec6",
+    "url": "/TicTacToe/static/js/runtime~main.756be40f.js"
+  },
+  {
+    "revision": "b55a0c6fd545476a9b93",
+    "url": "/TicTacToe/static/js/2.faf16e08.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/TicTacToe/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "1e2e201b52490c6976dbc75d9c3ede99",
+    "url": "/TicTacToe/index.html"
+  }
+];
